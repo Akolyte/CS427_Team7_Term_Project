7614971c6cfd410087ac3a26a44629214687c418
Kenny Li <63720387+MagicWaterBee@users.noreply.github.com>
2022-11-28 11:40:12 -0600

    3 test for login

--- app/src/androidTest/java/edu/uiuc/cs427app/LogInInstrumentedTest.java
+++ app/src/androidTest/java/edu/uiuc/cs427app/LogInInstrumentedTest.java
@@ -4,10 +4,15 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.ext.truth.content.IntentSubject.assertThat;
 
+import static org.hamcrest.Matchers.not;
+
+import android.accounts.AccountManager;
 import android.content.Context;
 import android.content.Intent;
 
@@ -16,63 +21,61 @@ import androidx.test.espresso.core.internal.deps.guava.collect.Iterables;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class LogInInstrumentedTest {
-    private String TEST_USER = "chris";
+    private String TEST_USER = "Adam";
+    private String TEST_PASSWORD = "123456";
     private int SLEEP_TIME = 1000;
 
-    private Intent initializeLogInIntent() {
-        Context ctx = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        Intent intent = new Intent();
-        intent.setClass(ctx, LoginActivity.class);
-        return intent;
-    }
-
-//    @Test
-//    public void BostonWeatherTest() throws Exception {
-//        initializeUserProvider("Boston");
-//        Intent intent = initializeIntent("Boston");
-//
-//        ActivityScenario.launch(intent);
-//        Thread.sleep(SLEEP_TIME);
-//        onView(withText("WEATHER")).perform(click());
-//        Thread.sleep(SLEEP_TIME);
-//
-//        onView(withId(R.id.welcomeText)).check(matches(withText("Welcome to Boston")));
-//        removeCityFromUserProvider("Boston");
-//    }
-//
-//    @Test
-//    public void OrlandoWeatherTest() throws Exception{
-//        initializeUserProvider("Orlando");
-//        Intent intent = initializeIntent("Orlando");
-//
-//        ActivityScenario.launch(intent);
-//        Thread.sleep(SLEEP_TIME);
-//        onView(withText("WEATHER")).perform(click());
-//        Thread.sleep(SLEEP_TIME);
-//
-//        onView(withId(R.id.welcomeText)).check(matches(withText("Welcome to Orlando")));
-//        removeCityFromUserProvider("Orlando");
-//    }
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityRule =
+            new ActivityTestRule<>(LoginActivity.class);
 
     @Test
     public void LogInTest1() throws Exception{
-        String test_name = "Adam";
-        String test_password = "123456";
-        Intent intent = initializeLogInIntent();
-        ActivityScenario.launch(intent);
+        onView(withId(R.id.userName)).perform(click()).perform(typeText(TEST_USER));
+        onView(withId(R.id.userName)).check(matches(withText(TEST_USER)));
         Thread.sleep(SLEEP_TIME);
-        onView(withId(R.id.userName)).perform(click()).perform(typeText(test_name));
-        onView(withId(R.id.userName)).check(matches(withText(test_name)));
-        Thread.sleep(SLEEP_TIME);
-        onView(withId(R.id.userPassword)).perform(click()).perform(typeText(test_password));
-        onView(withId(R.id.userPassword)).check(matches(withText(test_password)));
+        onView(withId(R.id.userPassword)).perform(click()).perform(typeText(TEST_PASSWORD));
+        onView(withId(R.id.userPassword)).check(matches(withText(TEST_PASSWORD)));
         Thread.sleep(SLEEP_TIME);
         onView(withId(R.id.buttonLogin)).perform(click());
+        onView(withText("Welcome back " + TEST_USER))
+                .inRoot(withDecorView(not(mActivityRule.getActivity().getWindow().getDecorView())))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void LogInErrorTest1() throws Exception{
+        onView(withId(R.id.userName)).perform(click()).perform(typeText(TEST_USER));
+        onView(withId(R.id.userName)).check(matches(withText(TEST_USER)));
+        Thread.sleep(SLEEP_TIME);
+        onView(withId(R.id.userPassword)).perform(click()).perform(typeText(TEST_PASSWORD+"1"));
+        onView(withId(R.id.userPassword)).check(matches(withText(TEST_PASSWORD+"1")));
+        Thread.sleep(SLEEP_TIME);
+        onView(withId(R.id.buttonLogin)).perform(click());
+        onView(withText(R.string.login_error))
+                .inRoot(withDecorView(not(mActivityRule.getActivity().getWindow().getDecorView())))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void LogInErrorTest2() throws Exception{
+        onView(withId(R.id.userName)).perform(click()).perform(typeText(TEST_USER+"1"));
+        onView(withId(R.id.userName)).check(matches(withText(TEST_USER+"1")));
+        Thread.sleep(SLEEP_TIME);
+        onView(withId(R.id.userPassword)).perform(click()).perform(typeText(TEST_PASSWORD));
+        onView(withId(R.id.userPassword)).check(matches(withText(TEST_PASSWORD)));
+        Thread.sleep(SLEEP_TIME);
+        onView(withId(R.id.buttonLogin)).perform(click());
+        onView(withText(R.string.login_error))
+                .inRoot(withDecorView(not(mActivityRule.getActivity().getWindow().getDecorView())))
+                .check(matches(isDisplayed()));
     }
 }
