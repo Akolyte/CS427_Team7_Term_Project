4c5efd1e3cf5930f4ec91688b21922e24a94f029
Ruhang Ma <m.ruhang@berkeley.edu>
2022-12-01 11:21:56 -0600

    Updated Signup Test With After Methods

--- app/src/androidTest/java/edu/uiuc/cs427app/UserSignupTest.java
+++ app/src/androidTest/java/edu/uiuc/cs427app/UserSignupTest.java
@@ -10,21 +10,22 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Context;
 import android.view.View;
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
 import static org.hamcrest.Matchers.not;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.sql.Timestamp;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -33,24 +34,30 @@ import java.sql.Timestamp;
  */
 @RunWith(AndroidJUnit4.class)
 public class UserSignupTest {
-    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-    private String USER1 = timestamp.toString();
-    private String PWD1 = "111";
-    private int SLEEP_TIME = 1000;
-
     @Rule
     public ActivityTestRule<LoginActivity> activityRule
             = new ActivityTestRule<>(LoginActivity.class);
 
-//    @Test
-//    public void useAppContext() {
-//        // Context of the app under test.
-//        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-//        assertEquals("edu.uiuc.cs427app", appContext.getPackageName());
-//    }
+    Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+    AccountManager am = AccountManager.get(appContext);
 
+    private String USER1 = "ruhangm2";
+    private String PWD1 = "123456";
+    Account account = new Account(USER1, "com.team7");
+    private final int SLEEP_TIME = 1000;
+
+    @After
+    public void CleanUserAfter() {
+        am.removeAccountExplicitly(account);
+    }
+
+    /**
+    * Test 2 cases:
+     * 1. Normal signup
+     * 2. After normal signup, signup the same account twice
+    * */
     @Test
-    public void NormalSignupTest() throws InterruptedException {
+    public void NormalAndExistSignupTest() throws InterruptedException {
         // Normal Signup
         onView(withId(R.id.userName)).perform(click()).perform(typeText(USER1));
         onView(withId(R.id.userPassword)).perform(click()).perform(typeText(PWD1));
@@ -78,6 +85,7 @@ public class UserSignupTest {
         Thread.sleep(SLEEP_TIME);
     }
 
+    // Test if input is valid
     @Test
     public void InputErrorSignupTest1() throws InterruptedException {
         onView(withId(R.id.userName)).perform(click()).perform(typeText(USER1));
@@ -90,6 +98,7 @@ public class UserSignupTest {
         Thread.sleep(SLEEP_TIME);
     }
 
+    // Test if input is valid
     @Test
     public void InputErrorSignupTest2() throws InterruptedException {
         onView(withId(R.id.userPassword)).perform(click()).perform(typeText(PWD1));
